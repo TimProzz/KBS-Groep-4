acdb698c8044bda47425d46656510f2f496fca7e
Tim Knobben <tknobben9140@landstede.nl>
2018-11-08 11:54:19 +0100

    bug fix
    bug fixje

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,11 +7,11 @@ $(document).ready(function() {
     $( ".tableHistory" ).each(function() {
         pageCount++;
         var classname = this.className;
-        var singleClassname = classname.replace('tableHistory ','');
+        var singleClassname = classname.replace('table table-bordered tableHistory ','');
         var idClassname = "#" + singleClassname;
         
         $(idClassname).click(function() {
-            //console.log("test");
+            console.log("test");
             var number = this.id;
             // number is name of div to show
             for(var i = 1; i <= pageCount; i++) {
