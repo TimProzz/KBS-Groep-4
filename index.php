4576dea3936aadb6eacd5f7ef4daca63037017fa
Jordy <hamstra.jordy@gmail.com>
2018-11-16 14:19:02 +0100

    bug
    tim verneukt de boel

--- index.php
+++ index.php
@@ -4,10 +4,7 @@
 ?>
 
 <?php
-    $stockItems = $pdo->query("SELECT * FROM StockItems S
-            JOIN stockitemstockgroups SG ON S.StockItemID = SG.StockItemID
-            JOIN stockgroups G ON SG.StockGroupID = G.StockGroupID
-            GROUP BY S.StockItemID");
+    $stockItems = $pdo->query("SELECT * FROM StockItems");
 ?>
 
 
