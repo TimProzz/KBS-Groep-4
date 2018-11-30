18f1a381820ea13a917dedd40d436a24c291bdb1
Tim Knobben <tknobben9140@landstede.nl>
2018-11-30 14:26:24 +0100

    Besteld is klaar
    bestellen is klaar

--- checkout.php
+++ checkout.php
@@ -39,11 +39,73 @@
             header("Location: account.php?error=Je moet eerst je NAW-gegevens invullen voordat je kan afrekenen!");
             exit;
         }
+        $accountDetails = $pdo->query("SELECT * FROM users WHERE username ='" . $_COOKIE['login'] . "'");
+        $accountDetails->execute();
+        $row = $accountDetails->fetch();
     }
 
-    $accountDetails = $pdo->query("SELECT * FROM users WHERE username ='" . $_COOKIE['login'] . "'");
-    $accountDetails->execute();
-    $row = $accountDetails->fetch();
+    if(isset($_POST["pay"])) {
+        if(!isset($_POST["paymentmethod"])) {
+            $errorMessage = "Selecteer een betaald methode!";
+        } else {
+            $products = $_COOKIE["winkelmand"];
+            $date = date("d-m-Y H:i:s");
+            $price = $_POST["price"];
+            $paymentMethod = $_POST["paymentmethod"];
+            $delivery_date = $_POST["fullDate"];
+            if(userLoggedIn()) {
+                $accountDetails = $pdo->query("SELECT id FROM users WHERE username ='" . $_COOKIE['login'] . "'");
+                $accountDetails->execute();
+                $row = $accountDetails->fetch();
+                $userID = $row["id"];
+
+                $query = $pdo->prepare("INSERT INTO get_order (customerID, products, date, price, paymentmethod, delivery_date) VALUES (:customerID, :products, :date, :price, :paymentmethod, :delivery_date)");
+                $query->bindValue(':customerID', $userID);
+                $query->bindValue(':products', $products);
+                $query->bindValue(':date', $date);
+                $query->bindValue(':price', $price);
+                $query->bindValue(':paymentmethod', $paymentMethod);
+                $query->bindValue(':delivery_date', $delivery_date);
+                $query->execute();
+            } else {
+                $email = $_POST["email"];
+                $voornaam = $_POST["voornaam"];
+                $tussenvoegsels = $_POST["tussenvoegsels"];
+                $achternaam = $_POST["achternaam"];
+                $straat = $_POST["straat"];
+                $huisnummer = $_POST["huisnummer"];
+                $woonplaats = $_POST["woonplaats"];
+                $postcode = $_POST["postcode"];
+                $telefoonnummer = $_POST["telefoonnummer"];
+
+                $query = $pdo->prepare("INSERT INTO guest_order (email, voornaam, tussenvoegsels, achternaam, straat, huisnummer, woonplaats, postcode, telefoonnummer) VALUES (:email, :voornaam, :tussenvoegsels, :achternaam, :straat, :huisnummer, :woonplaats, :postcode, :telefoonnummer)");
+                $query->bindValue(':email', $email);
+                $query->bindValue(':voornaam', $voornaam);
+                $query->bindValue(':tussenvoegsels', $tussenvoegsels);
+                $query->bindValue(':achternaam', $achternaam);
+                $query->bindValue(':straat', $straat);
+                $query->bindValue(':huisnummer', $huisnummer);
+                $query->bindValue(':woonplaats', $woonplaats);
+                $query->bindValue(':postcode', $postcode);
+                $query->bindValue(':telefoonnummer', $telefoonnummer);
+                $query->execute();
+                $guestID = $pdo->lastInsertId();
+
+                $query = $pdo->prepare("INSERT INTO get_order (guestID, products, date, price, paymentmethod, delivery_date) VALUES (:guestID, :products, :date, :price, :paymentmethod, :delivery_date)");
+                $query->bindValue(':guestID', $guestID);
+                $query->bindValue(':products', $products);
+                $query->bindValue(':date', $date);
+                $query->bindValue(':price', $price);
+                $query->bindValue(':paymentmethod', $paymentMethod);
+                $query->bindValue(':delivery_date', $delivery_date);
+                $query->execute();
+            }
+            unset($_COOKIE['winkelmand']);
+            setcookie("winkelmand", "", time()-3600);
+            header("Location: pay.php?success=Je hebt succesvol betaald! Je bestelling word klaar gemaakt!");
+            exit;
+        }
+    }
 ?>
 
 <?php
