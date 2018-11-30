013260bec2cacd31ee18e445fbd7ddb1c0128a95
fletterman <stijn.castrop@me.com>
2018-11-30 11:22:42 +0100

    Aanpassingen voor Register page

--- views/register.php
+++ views/register.php
@@ -1,12 +1,61 @@
-<div class="container push-padding">
-    <h2>Register</h2>
+<div class="container push-padding" style="width: 520px; padding: 50px 20px;">
+
+    <img src="assets/images/logo.png" class="img-fluid mx-auto d-block" alt="Logo" style="margin-bottom: 50px;">
+
     <form action="register.php" method="post">
-        <input type="text" name="username" placeholder="Username" value="<?php if (isset($username)) {
-    echo $username;
-} ?>" required><br>
-        <input type="password" name="password" placeholder="Password" required><br>
-        <input type="password" name="passwordCheck" placeholder="Password again" required><br>
-        <input type="submit" value="Submit" name="register">
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="Text" name="email" class="form-control" id="email" placeholder="Email" value="<?php if(isset($email)){echo $email;}?>" required>
+        </div>
+        <div class="form-group">
+            <label for="surname">Voornaam</label>
+            <input type="text" name="surname" class="form-control" id="surname" placeholder="Voornaam" value="<?php if(isset($surname)){echo $surname;}?>" required>
+        </div>
+        <div class="form-group">
+          <label for="infix">Tussenvoegsels</label>
+          <input type="text" name="infix" class="form-control" id="infix" placeholder="Tussenvoegsels" value="<?php if(isset($infix)){echo $infix;}?>">
+        </div>
+        <div class="form-group">
+          <label for="last name">Achternaam</label>
+          <input type="text" name="last name" class="form-control" id="last name" placeholder="Achternaam" value="<?php if(isset($last_name)){echo $last_name;}?>" required>
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-8">
+            <label for="street">Straat</label>
+            <input type="text" name="street" class="form-control" id="street" placeholder="Straat" value="<?php if(isset($street)){echo $street;}?>" required>
+          </div>
+          <div class="form-group col-md-4">
+            <label for="housenumber">Huisnummer</label>
+            <input type="text" name="housenumber" class="form-control" id="housenumber" placeholder="Huisnummer" value="<?php if(isset($housenumber)){echo $housenumber;}?>"required>
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-8">
+            <label for="city">Woonplaats</label>
+            <input type="text" name="city" class="form-control" id="city" placeholder="Woonplaats" value="<?php if(isset($city)){echo $city;}?>" required>
+          </div>
+          <div class="form-group col-md-4">
+            <label for="postal code">Postcode</label>
+            <input type="text" name="postal code" class="form-control" id="postal code" placeholder="Postcode" value="<?php if(isset($postal_code)){echo $postal_code;}?>"required>
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="phonenumber">Telefoonnummer</label>
+          <input type="text" name="phonenumber" class="form-control" id="postal code" placeholder="Telefoonnummer" value="<?php if(isset($phonenumber)){echo $phonenumber;}?>">
+        </div><br><br>
+        <div class="form-group">
+          <label for="username">Gebruikersnaam</label>
+          <input type="text" name="username" class="form-control" id="username" placeholder="Gebruikersnaam" required>
+        </div>
+        <div class="form-group">
+          <label for="password">Wachtwoord</label>
+          <input type="password" name="password" class="form-control" id="password" placeholder="Wachtwoord" required>
+        </div>
+        <div class="form-group">
+          <label for="passwordCheck">Herhaal wachtwoord</label>
+          <input type="password" name="passwordCheck" class="form-control" id="passwordCheck" placeholder="Herhaal wachtwoord" required>
+        </div>
+        <button type="submit" value="Submit" name="Registreer" class="btn btn-primary">Registreren</button>
     </form>
     <?php
     if (count($errorMessages) >= 1) {
@@ -14,9 +63,16 @@
             echo $value . "<br />";
         }
     }
+    ?>
+    <?php
+    if (count($errorMessages) >= 1) {
+      foreach ($errorMessages as $value) {
+        echo $value . "<br />";
+      }
+    }
 
     if (isset($registerSuccessful)) {
-        echo $registerSuccessful;
+      echo $registerSuccessful;
     }
     ?>
-</div>
+  </div>
